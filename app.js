54a781ebbe9f20388bcad5bf35bd8211741605ac
ocherisan <nekodevochka@mail.ru>
2021-11-25 00:24:05 +0500

    conntected to db

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const PORT = config.get("port") || 5000;
+
+const app = express();
+
+
+async function start(){
+    try{
+        await mongoose.connect(config.get('mongoUri'),{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        app.listen(PORT, ()=>{console.log(`App has been started on port ${PORT}`)});
+    }
+    catch (e){
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start();
+
+//db.createUser({user:"admin",pwd:"131281",roles:[{role:"clusterAdmin",db:"admin"},{role:"readAnyDatabase",db:"admin"},"readWrite"]},{w:"majority",wtimeout:5000})
