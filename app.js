e2a620becb9a3dd5e8e9e8c35d54f5477c5aadd5
ocherisan <nekodevochka@mail.ru>
2021-11-25 01:28:00 +0500

    added routes and handlers for auth

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
 
+
 const PORT = config.get("port") || 5000;
 
 const app = express();
 
+app.use('/api/auth',require('./routes/auth.routes'))
 
 async function start(){
     try{
---
+++ routes/auth.routes.js
@@ -0,0 +1,99 @@
+const { Router } = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("../config/default.json");
+
+const router = Router();
+
+//TODO: переделать с помощью passport.js
+// /api/auth/register
+router.post(
+  "/register",
+  [
+    check("email", "Incorrect email").isEmail(),
+    check("password", "Minimal password length - 6 symbols").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Incorrect data during registration",
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "User with this email already exists" });
+      }
+      const hashedPasssword = await bcrypt.hash(password, 12);
+      const newUser = new User({ email, password: hashedPasssword });
+
+      await newUser.save();
+
+      res.status(201).json({ message: "User is created" });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong. Try again" });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    check("email", "Enter correct email").normalizeEmail().isEmail(),
+    check("password", "Enter password").exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Incorrect data during registration",
+        });
+      }
+
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "User does not exist" });
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordMatch) {
+        return res
+          .status(400)
+          .json({ message: "Wrong cridentials. Try again" });
+      }
+
+      const token = jwt.sign(
+        {
+          userId: user.id,
+        },
+        config.get("jwtSecret"),
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      res.json({ token, userId: user.id });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong. Try again" });
+    }
+  }
+);
+
+module.exports = router;
